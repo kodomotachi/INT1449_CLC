aa1a502ca82015e03e471ee943d2c577239475eb
kodomotachi <kodomotachi2k4@gmail.com>
2025-11-23 09:54:52 +0700

    Revert "adjust permission and add "LocationService""
    This reverts commit 6d7263d6

--- app/src/main/java/com/example/weatherapp/model/NominatimCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.weatherapp.model;
-
-import org.json.JSONArray;
-
-public interface NominatimCallback {
-    void onSuccess(JSONArray result);
-    void onError(String message);
-}
